c32ba43b8e8b7cdf14e20496f938572f22cb694e
Jodi-Ann Moore-Barrett <moorebarrett.jodiann@gmail.com>
2022-11-30 13:29:47 -0600

    Add Word Generator on Keydown

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -17,7 +17,7 @@ const timer = select('.timer span');
 const word = select ('.word');
 const userInput = select ('.user-input');
 const startButton = select('.start');
-const stopButton = select('.start');
+const stopButton = select('.stop');
 
 const wordBank = ['dinosaur', 'love', 'pineapple', 'calendar', 'robot', 'building', 'population',
 'weather', 'bottle', 'history', 'dream', 'character', 'money', 'absolute',
@@ -38,40 +38,82 @@ let shuffledWordBank = [...shuffle(wordBank)];
 let processedWords = [];
 let currentWord = '';
 
+/**-------------------------------------------------------------------------- */
+
+/**---------------------Game Functions and Events---------------------------- */
+
+
+
+// function to compare the user input to the random word selected
+function compareValues (currWord, input) {
+    // clean strings for comparison
+    currWord.toLowerCase().trim();
+    input.toLowerCase().trim();
+
+    print(currWord);
+    print(input);
+    if(currWord === input) {
+        print('here');
+        focusInput();
+        generateWord();
+    }
+}
+
+// start timer
 function updateTimer() {
     let timeleft = 99;
     let timeValue = setInterval(function(){
     if(timeleft <= 0){
+        // stop timer on expiry
         clearInterval(timeValue);
+        // time has expired, calculate player's score
+        generateScore();
+        focusInput();
     }
     timer.innerText = timeleft;
     timeleft -= 1;
     }, 1000);
 }
 
+// function to shuffle items in an array
 function shuffle(array) {
     return array.sort(() => Math.random() - 0.5);
 }
 
-// function to generate a random word from the array for user to type
+// function to generate a random word from the shuffledWordBank for player to type
 function generateWord() {
-
-    let originalLength = shuffledWordBank.length;
-
-    if(originalLength.length !== 0) {
-        let i = Math.floor(Math.random() * originalLength);
-        word.innerText = shuffledWordBank[i];
-        currentWord = shuffledWordBank[i];
-        processedWords.push(shuffledWordBank[i]);
-        shuffledWordBank.splice(i, 1);
+    if(shuffledWordBank.length !== 0) {
+        let originalLength = shuffledWordBank.length;
+    
+        if(originalLength.length !== 0) {
+            // grab a random index in the array
+            let i = Math.floor(Math.random() * originalLength); 
+            // display word to player
+            word.innerText = shuffledWordBank[i];
+            // store selected word in a global variable
+            currentWord = shuffledWordBank[i];
+            // add selected word to an array of words that have been selected
+            processedWords.push(shuffledWordBank[i]);
+            // remove the word from original array once selected
+            shuffledWordBank.splice(i, 1);
+        }
+    
+        print(processedWords);
+        print(processedWords.length);
+        print(shuffledWordBank.length);
+    } else {
+        // player has exhausted all words so calculate their score
+        generateScore();
     }
-
-    print(processedWords);
-    print(processedWords.length);
-    print(shuffledWordBank.length);
 }
 
-// compare the user input with the current word selected from the bank
+function focusInput() {
+    userInput.value = '';
+    userInput.focus();
+    userInput.scrollIntoView();
+}
+
+// compare the player input with the current word selected from the bank
 onEvent('keyup', userInput, function() {
     compareValues(currentWord, userInput.value);
 });
@@ -79,18 +121,17 @@ onEvent('keyup', userInput, function() {
 
 // start game when start button is clicked
 onEvent('click', startButton, function() {
-    startButton.innerText = 'STOP';
-    startButton.classList.remove('start');
-    startButton.classList.add('stop');
+    startButton.style.display = 'none';
+    stopButton.style.display = 'block';
+    focusInput();
     updateTimer();
     generateWord();
 });
 
 // stop game when stop button is clicked
 onEvent('click', stopButton, function() {
-    stopButton.innerText = 'Click to Start';
-    stopButton.classList.remove('stop');
-    stopButton.classList.add('start');
+    stopButton.style.display = 'none';
+    startButton.style.display = 'block';
     updateTimer();
 });
   
@@ -99,6 +140,6 @@ onEvent('click', stopButton, function() {
 onEvent('load', window, () => {
     userInput.value = '';
     word.innerText = '';
-    userInput.focus();
-    userInput.scrollIntoView();
+    focusInput();
 });
+
